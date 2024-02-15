d8a356cab3853ead5042b2dc4769b73b633139d7
Dmitry Averin <avedmigen@gmail.com>
2024-02-15 15:24:31 +0300

    2.3 Cоздаёт компоненты для всех страниц приложения

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,12 +2,10 @@ import MainScreen from '../../pages/main-screen/main-screen';
 
 type AppScreenProps = {
   favoriteCount: number;
-}
+};
 
-function App({favoriteCount}: AppScreenProps): JSX.Element {
-  return (
-    <MainScreen favoriteCount={favoriteCount} />
-  );
+function App({ favoriteCount }: AppScreenProps): JSX.Element {
+  return <MainScreen favoriteCount={favoriteCount} />;
 }
 
 export default App;
