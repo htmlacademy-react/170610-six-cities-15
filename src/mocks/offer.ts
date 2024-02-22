5d98b06a265d338a72b7340f53d435292b2a1534
Dmitry Averin <avedmigen@gmail.com>
2024-02-22 18:17:28 +0300

    Добавит в моки координаты объектов для аренды. В качестве координат использует пары (широта, долгота)

--- src/mocks/offer.ts
+++ src/mocks/offer.ts
@@ -3,30 +3,37 @@ import { v4 as uuidv4 } from 'uuid';
 import { Offer } from '../types/offer';
 import capitalize from './utils';
 
+function getRandomCoordinates(): { latitude: number; longitude: number } {
+  const coordinates = [
+    { latitude: 52.3909553943508, longitude: 4.85309666406198 },
+    { latitude: 52.3609553943508, longitude: 4.85309666406198 },
+    { latitude: 52.3909553943508, longitude: 4.929309666406198 },
+    { latitude: 52.3809553943508, longitude: 4.939309666406198 },
+  ];
+
+  const randomIndex = Math.floor(Math.random() * coordinates.length);
+  return coordinates[randomIndex];
+}
+
 function generateOfferMock(): Offer {
+  const { latitude, longitude } = getRandomCoordinates();
+
   return {
     id: uuidv4(),
     title: capitalize(faker.lorem.words()),
     type: faker.random.arrayElement(['apartment', 'house', 'room']),
     price: faker.datatype.number({ min: 50, max: 300 }),
     city: {
-      name: faker.random.arrayElement([
-        'Paris',
-        'Cologne',
-        'Brussels',
-        'Amsterdam',
-        'Hamburg',
-        'Dusseldorf',
-      ]),
+      name: faker.random.arrayElement(['Amsterdam']),
       location: {
-        latitude: parseFloat(faker.address.latitude()),
-        longitude: parseFloat(faker.address.longitude()),
+        latitude: 52.379189,
+        longitude: 4.899431,
         zoom: faker.datatype.number({ min: 8, max: 12 }),
       },
     },
     location: {
-      latitude: parseFloat(faker.address.latitude()),
-      longitude: parseFloat(faker.address.longitude()),
+      latitude,
+      longitude,
       zoom: faker.datatype.number({ min: 8, max: 12 }),
     },
     isFavorite: faker.datatype.boolean(),
