ceb378e761a9c29d0d4901cb4ee2aa0102bbbcd1
Dmitry Averin <avedmigen@gmail.com>
2024-02-24 19:42:07 +0300

    Рефакторинг экшенов

--- src/store/action.ts
+++ src/store/action.ts
@@ -2,15 +2,9 @@ import { createAction } from '@reduxjs/toolkit';
 import { OfferWithComments } from '../types/offerWithComments';
 
 // Определяем тип для действия
-export type SetAllOffersAction = ReturnType<typeof setAllOffers>;
 export type SetActiveCityAction = ReturnType<typeof setActiveCity>;
+export type SetAllOffersAction = ReturnType<typeof setAllOffers>;
 
 // Создаем действие
-export const setAllOffers = createAction<OfferWithComments[]>('setAllOffers');
 export const setActiveCity = createAction<string>('setActiveCity');
-
-export type State = {
-  city: string;
-  allOffers: OfferWithComments[];
-  cityOffers: OfferWithComments[];
-};
+export const setAllOffers = createAction<OfferWithComments[]>('setAllOffers');
