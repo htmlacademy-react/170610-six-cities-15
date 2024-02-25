fada213bc7cb64b8333f0e866476f9fa159c81a4
Dmitry Averin <avedmigen@gmail.com>
2024-02-25 12:25:48 +0300

    Делает колбек onOfferHover в списке оферов опциональным

--- src/components/offers-list/offers-list.tsx
+++ src/components/offers-list/offers-list.tsx
@@ -4,13 +4,13 @@ import { OfferWithComments } from '../../types/offerWithComments';
 type OffersListProps = {
   offers: OfferWithComments[];
   className?: string;
-  onOfferHover: (offerId: string) => void;
+  onOfferHover?: (offerId: string) => void; // Сделаем onOfferHover опциональным добавив знак вопроса
 };
 
 function OffersList({
   offers,
   className,
-  onOfferHover,
+  onOfferHover = () => {}, // Устанавливаем значение по умолчанию как пустая функция
 }: OffersListProps): JSX.Element {
   return (
     <div className={`${className}`}>
@@ -18,7 +18,7 @@ function OffersList({
         <Card
           key={offer.id}
           offer={offer}
-          isActive={false} // Не забудьте передать этот проп, если он используется в компоненте Card
+          isActive={false} // Устанавливаем isActive по умолчанию как false
           onOfferHover={onOfferHover} // Передаем колбэк onOfferHover для обработки наведения мыши на карточку
         />
       ))}
