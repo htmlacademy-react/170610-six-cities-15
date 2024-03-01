5fbd72fda6a51978d9bb3c345b85879abba960ce
Dmitry Averin <avedmigen@gmail.com>
2024-03-01 20:30:53 +0300

    По клику на город в избранном открывает офферы для города на главной

--- src/components/locations-item/locations-item.tsx
+++ src/components/locations-item/locations-item.tsx
@@ -1,14 +1,21 @@
 import { Link } from 'react-router-dom';
+import { setActiveCity } from '../../store/action';
+import { store } from '../../store';
+
 type LocationsItemProps = {
   city: string;
 };
+
+function handleCityClick(city: string) {
+  store.dispatch(setActiveCity(city));
+}
 
 function LocationsItem({ city }: LocationsItemProps): JSX.Element {
   return (
     <div className="favorites__locations locations locations--current">
       <div className="locations__item">
         <Link className="locations__item-link" to={`/?city=${city}`}>
-          <span>{city}</span>
+          <span onClick={() => handleCityClick(city)}>{city}</span>
         </Link>
       </div>
     </div>
