04747961a15b0e60e847f4a378b9446c8e10b32c
Dmitry Averin <avedmigen@gmail.com>
2024-03-09 19:41:01 +0300

    Переводит LocationsItem на слайсы

--- src/components/locations-item/locations-item.tsx
+++ src/components/locations-item/locations-item.tsx
@@ -1,21 +1,24 @@
 import { Link } from 'react-router-dom';
-import { setActiveCity } from '../../store/action';
-import { store } from '../../store';
+import { AppRoute } from '../../const';
+import { useAppDispatch } from '../../hooks/index';
+import { changeCity } from '../../store/app-process/app-process.slice';
 
 type LocationsItemProps = {
   city: string;
 };
 
-function handleCityClick(city: string) {
-  store.dispatch(setActiveCity(city));
-}
-
 function LocationsItem({ city }: LocationsItemProps): JSX.Element {
+  const dispatch = useAppDispatch();
+
+  function handleCityClick() {
+    dispatch(changeCity({ city }));
+  }
+
   return (
     <div className="favorites__locations locations locations--current">
       <div className="locations__item">
-        <Link className="locations__item-link" to={`/?city=${city}`}>
-          <span onClick={() => handleCityClick(city)}>{city}</span>
+        <Link className="locations__item-link" to={AppRoute.Main}>
+          <span onClick={() => handleCityClick()}>{city}</span>
         </Link>
       </div>
     </div>
