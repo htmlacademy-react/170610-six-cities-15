3e1ca607be1006b5060e38c19d93c3dbf83f6c8e
Dmitry Averin <avedmigen@gmail.com>
2024-03-09 19:01:35 +0300

    Исправляет предыдущий коммит

--- src/store/middlewares/redirect.ts
+++ src/store/middlewares/redirect.ts
@@ -1,12 +1,10 @@
 import { PayloadAction } from '@reduxjs/toolkit';
-import { Middleware } from 'redux';
 import browserHistory from '../../browser-history';
+import { Middleware } from 'redux';
+import { rootReducer } from '../root-reducer';
 
-type Reducer = {
-  app: {
-    redirectToRoute: (action: PayloadAction<string>) => void;
-  };
-};
+type Reducer = ReturnType<typeof rootReducer>;
+
 export const redirect: Middleware<unknown, Reducer> =
   () => (next) => (action: PayloadAction<string>) => {
     if (action.type === 'app/redirectToRoute') {
