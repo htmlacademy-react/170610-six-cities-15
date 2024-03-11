2d927de30735c892d081cc0dd796ac8f9043b113
Dmitry Averin <avedmigen@gmail.com>
2024-03-11 08:45:24 +0300

    Конвертирует RatingInput в Function Expression

--- src/components/common/rating-input/rating-input.tsx
+++ src/components/common/rating-input/rating-input.tsx
@@ -5,37 +5,39 @@ type RatingInputProps = {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   checked: boolean;
   title: string;
-  disabled: boolean; // Проп для управления доступностью компонента
+  disabled: boolean;
 };
 
-const RatingInput: React.FC<RatingInputProps> = ({
+function RatingInput({
   value,
   onChange,
   checked,
   title,
-  disabled, // Передаем проп disabled
-}) => (
-  <>
-    <input
-      className="form__rating-input visually-hidden"
-      name="rating"
-      value={value}
-      id={`${value}-stars`}
-      type="radio"
-      onChange={onChange}
-      checked={checked}
-      disabled={disabled} // Используем проп disabled для управления доступностью компонента
-    />
-    <label
-      htmlFor={`${value}-stars`}
-      className="reviews__rating-label form__rating-label"
-      title={title}
-    >
-      <svg className="form__star-image" width="37" height="33">
-        <use xlinkHref="#icon-star"></use>
-      </svg>
-    </label>
-  </>
-);
+  disabled,
+}: RatingInputProps): JSX.Element {
+  return (
+    <>
+      <input
+        className="form__rating-input visually-hidden"
+        name="rating"
+        value={value}
+        id={`${value}-stars`}
+        type="radio"
+        onChange={onChange}
+        checked={checked}
+        disabled={disabled}
+      />
+      <label
+        htmlFor={`${value}-stars`}
+        className="reviews__rating-label form__rating-label"
+        title={title}
+      >
+        <svg className="form__star-image" width="37" height="33">
+          <use xlinkHref="#icon-star"></use>
+        </svg>
+      </label>
+    </>
+  );
+}
 
 export default RatingInput;
