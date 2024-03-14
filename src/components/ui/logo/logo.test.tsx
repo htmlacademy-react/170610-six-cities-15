d88e2f2fc09c9db3c254d847781acb516c741de2
Dmitry Averin <avedmigen@gmail.com>
2024-03-14 10:53:58 +0300

    Уточняет позитивный тест для Logo и добавляет негативный без активного класса

--- src/components/ui/logo/logo.test.tsx
+++ src/components/ui/logo/logo.test.tsx
@@ -1,16 +1,49 @@
 import { render, screen } from '@testing-library/react';
+import { expect, it } from 'vitest';
+import { AppRoute } from '../../../const';
 import { withHistory } from '../../../utils/mock-component';
 import Logo from './logo';
 
 describe('Component: Logo', () => {
-  it('should render correctly', () => {
+  it('should render a Link component with the correct class and to prop', () => {
+    const classPrefix = 'logo';
+    const width = '100px';
+    const height = '50px';
+    const isMainPage = true;
     const expectedAltText = '6 cities logo';
     const preparedComponent = withHistory(
-      <Logo classPrefix="logo" width="30" height="30" />
+      <Logo
+        classPrefix={classPrefix}
+        width={width}
+        height={height}
+        isMainPage={isMainPage}
+      />
     );
 
     render(preparedComponent);
 
+    const linkElement = screen.getByRole('link');
+    const expectedClassName = `${classPrefix}__logo-link ${
+      isMainPage ? `${classPrefix}__logo-link--active` : ''
+    }`;
+
+    expect(linkElement).toHaveClass(expectedClassName);
+    expect(linkElement).toHaveAttribute('href', AppRoute.Main);
     expect(screen.getByAltText(expectedAltText)).toBeInTheDocument();
+  });
+  it('should render a Link component without the active class when isMainPage is undefined', () => {
+    const classPrefix = 'logo';
+    const width = '100px';
+    const height = '50px';
+
+    const preparedComponent = withHistory(
+      <Logo classPrefix={classPrefix} width={width} height={height} />
+    );
+
+    render(preparedComponent);
+
+    const linkElement = screen.getByRole('link');
+    expect(linkElement).toHaveClass(`${classPrefix}__logo-link`);
+    expect(linkElement).toHaveAttribute('href', AppRoute.Main);
   });
 });
