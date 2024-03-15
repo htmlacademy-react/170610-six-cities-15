1b2859ac0de6eb2b1a114b4ae61f630bc22ba654
Dmitry Averin <avedmigen@gmail.com>
2024-03-15 16:44:58 +0300

    Рефакторинг app-process.selectors.test

--- src/store/app-process/app-process.selectors.test.ts
+++ src/store/app-process/app-process.selectors.test.ts
@@ -4,16 +4,17 @@ import { getRandomCityName } from '../../utils/common';
 import { getCity } from './app-process.selectors';
 
 describe('AppProcess selectors', () => {
+  const mockCity = getRandomCityName(citiesNames);
+  const mockState = {
+    [NameSpace.App]: {
+      city: mockCity,
+    },
+  };
   describe('getCity', () => {
     it('should return correct city', () => {
-      const mockCity = getRandomCityName(citiesNames);
-      const mockState = {
-        [NameSpace.App]: {
-          city: mockCity,
-        },
-      };
+      const { city } = mockState[NameSpace.App];
       const result = getCity(mockState as Pick<TState, NameSpace.App>);
-      expect(result).toEqual(mockCity);
+      expect(result).toEqual(city);
     });
   });
 });
