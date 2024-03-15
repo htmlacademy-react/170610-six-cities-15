2c51a449a80878b925e211d391747ca5280723ec
Dmitry Averin <avedmigen@gmail.com>
2024-03-15 13:57:56 +0300

    Добавляет тест для user-process.selectors - getAuthCheckedStatus

--- src/store/user-process/user-process.selectors.test.ts
+++ src/store/user-process/user-process.selectors.test.ts
@@ -1,6 +1,9 @@
 import { AuthorizationStatus, NameSpace } from '../../const';
 import { TState } from '../../types/state';
-import { getAuthorizationStatus } from './user-process.selectors';
+import {
+  getAuthorizationStatus,
+  getAuthCheckedStatus,
+} from './user-process.selectors';
 describe('getAuthorizationStatus', () => {
   it('should return correct authorization status', () => {
     const mockState = {
@@ -12,5 +15,19 @@ describe('getAuthorizationStatus', () => {
       mockState as Pick<TState, NameSpace.User>
     );
     expect(result).toBe(AuthorizationStatus.Auth);
+  });
+});
+
+describe('getAuthCheckedStatus', () => {
+  it('should return true when user authorization status is not unknown', () => {
+    const mockState = {
+      [NameSpace.User]: {
+        authorizationStatus: AuthorizationStatus.Auth,
+      },
+    };
+    const result = getAuthCheckedStatus(
+      mockState as Pick<TState, NameSpace.User>
+    );
+    expect(result).toBe(true);
   });
 });
