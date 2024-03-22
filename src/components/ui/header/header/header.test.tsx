7e2cad6d841fd4a712e8e4c654c5d339b427091c
Dmitry Averin <avedmigen@gmail.com>
2024-03-22 10:31:02 +0300

    Добавит базовый тест рендера для компонента Header

---
+++ src/components/ui/header/header/header.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryHistory, createMemoryHistory } from 'history';
+import { withHistory, withStore } from '../../../../utils/mock-component';
+import { makeFakeStore } from '../../../../utils/mocks';
+import Header from './header';
+
+describe('Component: Header', () => {
+  let mockHistory: MemoryHistory;
+
+  beforeEach(() => {
+    mockHistory = createMemoryHistory();
+  });
+
+  it('should render correctly', () => {
+    const testId = 'header';
+    const withHistoryComponent = withHistory(<Header />, mockHistory);
+    const { withStoreComponent } = withStore(
+      withHistoryComponent,
+      makeFakeStore()
+    );
+
+    render(withStoreComponent);
+
+    expect(screen.getByTestId(testId)).toBeInTheDocument();
+  });
+});
--- src/components/ui/header/header/header.tsx
+++ src/components/ui/header/header/header.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { AppRoute, AuthorizationStatus } from '../../../../const';
+import { useAppDispatch, useAppSelector } from '../../../../hooks';
 import {
   fetchFavoriteOffersAction,
   fetchUserDataAction,
@@ -11,7 +12,6 @@ import {
   getAuthorizationStatus,
   getUserData,
 } from '../../../../store/user-process/user-process.selectors';
-import { useAppDispatch, useAppSelector } from '../../../../hooks';
 import Logo from '../../logo/logo';
 import HeaderNav from '../header-nav/header-nav';
 import UnauthenticatedUser from '../unauthenticated-user/unauthenticated-user';
@@ -74,7 +74,7 @@ function Header() {
   );
 
   return (
-    <header className="header">
+    <header className="header" data-testid="header">
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
