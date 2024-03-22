f921e89984438350a91ef04b7c9473987801ad2c
Dmitry Averin <avedmigen@gmail.com>
2024-03-22 10:21:25 +0300

    Добавит базовый тест рендера для компонента Footer

--- src/components/ui/footer/footer.tsx
+++ src/components/ui/footer/footer.tsx
@@ -2,7 +2,7 @@ import Logo from '../../ui/logo/logo';
 
 function Footer() {
   return (
-    <footer className="footer container">
+    <footer className="footer container" data-testid="footer">
       <Logo classPrefix="footer" width="64" height="33" />
     </footer>
   );
