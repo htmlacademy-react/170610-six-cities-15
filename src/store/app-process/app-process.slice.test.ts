5dfa69624f6035036c71305e0f66827197deca89
Dmitry Averin <avedmigen@gmail.com>
2024-03-23 09:26:36 +0300

    Добавит тесты для AppProcess Slice

---
+++ src/store/app-process/app-process.slice.test.ts
@@ -0,0 +1,35 @@
+import { Cities, citiesNames, DEFAULT_CITY_NAME } from '../../const';
+import { getRandomCityName } from '../../utils/common';
+import { appProcess, changeCity } from './app-process.slice';
+
+describe('AppProcess Slice', () => {
+  it('should return initial state with empty action', () => {
+    const emptyAction = { type: '' };
+    const expectedState = { city: getRandomCityName(citiesNames) };
+
+    const result = appProcess.reducer(expectedState, emptyAction);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should return default initial state with empty action', () => {
+    const emptyAction = { type: '' };
+    const expectedState = { city: DEFAULT_CITY_NAME };
+
+    const result = appProcess.reducer(undefined, emptyAction);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should change city with "changeCity" action', () => {
+    const initialState = { city: DEFAULT_CITY_NAME };
+    const expectedState = { city: Cities.Amsterdam };
+
+    const result = appProcess.reducer(
+      initialState,
+      changeCity({ city: Cities.Amsterdam })
+    );
+
+    expect(result).toEqual(expectedState);
+  });
+});
