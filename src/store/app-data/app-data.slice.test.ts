44a115cb86555da05c025171afeec2f6982f25cf
Dmitry Averin <avedmigen@gmail.com>
2024-03-23 10:57:02 +0300

    Добавит тесты для AppData Slice -  WIP - fetchOffersAction

--- src/store/app-data/app-data.slice.test.ts
+++ src/store/app-data/app-data.slice.test.ts
@@ -1,4 +1,6 @@
 import { DEFAULT_STATE } from '../../const';
+import { getRandomNumber, makeFakeOffer } from '../../utils/mocks';
+import { fetchOffersAction } from '../api-actions';
 import { appData } from './app-data.slice';
 
 describe('AppData Slice', () => {
@@ -20,6 +22,50 @@ describe('AppData Slice', () => {
     };
 
     const result = appData.reducer(undefined, emptyAction);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "isOffersDataLoading" to "true", "hasError" to "false" with "fetchOffersAction.pending"', () => {
+    const expectedState = {
+      ...DEFAULT_STATE.DATA,
+      isOffersDataLoading: true,
+      hasError: false,
+    };
+
+    const result = appData.reducer(undefined, fetchOffersAction.pending);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "offers" to array with offers, "isOffersDataLoading" to "false" with "fetchOffersAction.fulfilled"', () => {
+    const mockOffer = makeFakeOffer();
+    const mockOffers = Array.from(
+      { length: getRandomNumber(1, 15) },
+      () => mockOffer
+    );
+    const expectedState = {
+      ...DEFAULT_STATE.DATA,
+      offers: mockOffers,
+      isOffersDataLoading: false,
+      hasError: false,
+    };
+
+    const result = appData.reducer(
+      undefined,
+      fetchOffersAction.fulfilled(mockOffers, '', undefined)
+    );
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "isOffersDataLoading" to "false", "hasError" to "true" with "fetchOffersAction.rejected', () => {
+    const expectedState = {
+      ...DEFAULT_STATE.DATA,
+      isOffersDataLoading: false,
+      hasError: true,
+    };
+    const result = appData.reducer(undefined, fetchOffersAction.rejected);
 
     expect(result).toEqual(expectedState);
   });
